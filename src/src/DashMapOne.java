c5a2eaa8f49ad8bae3bee204f2aa83d82b154b23
Zanetta Norris <zanettanorris@gmail.com>
2020-04-03 00:49:57 -0400

    pom file fixed. some code added to maps.

---
+++ src/src/DashMapOne.java
@@ -0,0 +1,54 @@
+public class DashMapOne implements HashMapX {
+    public class StringSET {
+        private static final int R = 128;
+        private Node root = new Node();
+
+        private class Node {
+            Node[] next = new Node[R];
+            boolean end;
+        }
+
+        public boolean contains(String s) {
+            return contains(root, s, 0)
+        }
+
+        private boolean contains(Node x, String s, int i) {
+            if (x == null) return false;
+            if (i == s.length()) return x.end;
+            char c = s.charAt(i);
+            System.out.println(x);
+            return contains(x.next[c], s, i + 1);
+        }
+
+        public String dashFunctionOne(String key) {
+            if (key.length() > 0) {
+                return String.valueOf(key.charAt(0)).toLowerCase();
+            }
+            return null;
+        }
+    }
+
+    public void set(String key, Integer value) {
+    }
+
+    public void delete(String key) {
+
+    }
+
+    public Integer get(String key) {
+        return null;
+    }
+
+    public Boolean isEmpty() {
+        return null;
+    }
+
+    public Integer size() {
+        return null;
+    }
+
+    public Integer bucketSize(String key) {
+        return null;
+    }
+
+}
---
+++ src/src/DashMapThree.java
@@ -0,0 +1,45 @@
+public class DashMapThree implements HashMapX{
+
+    public class StringSET{
+        private static final int R= 128;
+        private Node root = new Node();
+        private class Node {
+            Node[] next = new Node[R];
+            boolean end;
+        }
+        public boolean contains (String s) {
+            return contains (root, s, 0);
+        }
+        private boolean contains (Node x, String s, int i) {
+            if (x == null) return false;
+            if (i == s.length()) return x.end;
+            char c = s.charAt(i);
+            System.out.println(x);
+            return contains(x.next[c], s, i + 1);
+        }
+        public String dashFunctionThree(String key) {
+            if (key.length() > 1) {
+                return String.valueOf(key.charAt(0)).toLowerCase() + String.valueOf(key.charAt(1)).toLowerCase();
+            }
+            return null;
+        }
+    }
+    public void set(String key, Integer value) {
+
+    }
+    public void delete (String key){
+
+    }
+    public Integer get(String key){
+        return null;
+    }
+    public Boolean isEmpty(){
+        return null;
+    }
+    public Integer size(){
+        return null;
+    }
+    public Integer bucketSize(String key) {
+        return null;
+    }
+}
